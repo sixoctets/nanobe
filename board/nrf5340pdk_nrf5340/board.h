43c5b57a30d5e86418f354cb945d4b046424ffd2
Vinayak Kariappa Chettimada <vinayak.chettimada@sixoctets.com>
2020-05-09 16:49:44 +0200

    board: added nrf5340pdk definition.

---
+++ board/nrf5340pdk_nrf5340/board.h
@@ -0,0 +1,11 @@
+#ifndef _BOARD_H_
+#define _BOARD_H_
+
+#define LED_GREEN 30 /* P0.30 */
+#define LED_BLUE  31 /* P0.31 */
+#define LED_RED   29 /* P0.29 */
+#define LED1      28 /* P0.28 */
+
+#define LED_BLINK (LED1)
+
+#endif
