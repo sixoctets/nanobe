36f46ece994d977b2b5f26274d5706fb892da375
Vinayak Kariappa Chettimada <vinayak.chettimada@sixoctets.com>
2020-05-10 08:18:29 +0200

    board: added nrf51dk definition.

---
+++ board/nrf51dk_nrf51822/board.h
@@ -0,0 +1,11 @@
+#ifndef _BOARD_H_
+#define _BOARD_H_
+
+#define LED_GREEN 17 /* P0.xx */
+#define LED_BLUE  18 /* P0.xx */
+#define LED_RED   16 /* P0.xx */
+#define LED1      16 /* P0.xx */
+
+#define LED_BLINK (LED1)
+
+#endif
